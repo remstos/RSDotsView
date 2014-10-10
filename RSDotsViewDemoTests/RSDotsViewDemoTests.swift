8650ef38996950198a1f998d47379a4c15a5623b
KemCake <santos.remi@icloud.com>
2014-10-10 11:09:44 +0200

    Initial Commit

---
+++ RSDotsViewDemoTests/RSDotsViewDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  RSDotsViewDemoTests.swift
+//  RSDotsViewDemoTests
+//
+//  Created by Remi Santos on 10/10/2014.
+//  Copyright (c) 2014 Remi Santos. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RSDotsViewDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
